e88f4842a27bc391468aa60be22a4e04670dab78
DecimTheJudge <paolo1piga@gmail.com>
2024-06-04 15:53:48 +0200

    prezzo biglietto

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// anni
+let anni = prompt("Quanti anni ha?")
+
+
+console.log(anni);
+// percorso
+let percorso = prompt("Quanti kilometri desidera percorrere?");
+
+
+// costo
+const costoKm = 0.21;
+
+console.log(percorso);
+
+// prezzo biglietto
+let prezzo = costoKm * percorso;
+
+console.log("il biglietto costa: ", prezzo);
+
